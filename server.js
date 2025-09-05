578ef99d12493e54b5dbf8f400eb1f89b0966250
happytimes121 <drakovish123@gmail.com>
2025-09-05 05:53:08 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,31 +1,40 @@
-// server.js
 const http = require("http");
+const fs = require("fs");
+const path = require("path");
 const WebSocket = require("ws");
 
-// Railway provides the port via environment variable
 const port = process.env.PORT || 3000;
 
-// Create a basic HTTP server
+// Create HTTP server
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("WebSocket server running\n");
+  if (req.url === "/" || req.url === "/client.html") {
+    // Serve the client.html file
+    const filePath = path.join(__dirname, "client.html");
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        res.writeHead(500);
+        res.end("Error loading client.html");
+      } else {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(data);
+      }
+    });
+  } else {
+    res.writeHead(404);
+    res.end("Not found");
+  }
 });
 
-// Attach WebSocket server to HTTP server
+// WebSocket server
 const wss = new WebSocket.Server({ server });
 
-// Handle new connections
 wss.on("connection", (socket) => {
   console.log("New player connected");
-
-  // Send welcome message to the new client
   socket.send("Welcome to the multiplayer server!");
 
-  // Listen for messages from clients
   socket.on("message", (message) => {
     console.log("Received:", message.toString());
-
-    // Broadcast the message to all connected clients
+    // Broadcast to all clients
     wss.clients.forEach((client) => {
       if (client.readyState === WebSocket.OPEN) {
         client.send(message.toString());
@@ -33,13 +42,9 @@ wss.on("connection", (socket) => {
     });
   });
 
-  // Handle client disconnect
-  socket.on("close", () => {
-    console.log("Player disconnected");
-  });
+  socket.on("close", () => console.log("Player disconnected"));
 });
 
-// Start the server
 server.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
